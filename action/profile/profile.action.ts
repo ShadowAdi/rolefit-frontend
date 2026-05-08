f24425b20d8b0399852216e7152b265169eb1708
ShadowAdi <shadowshukla76@gmail.com>
2026-05-08 12:10:56 +0530

    added get profile

--- action/profile/profile.action.ts
+++ action/profile/profile.action.ts
@@ -32,7 +32,7 @@ export const createProfile = async (
 
     return {
       success: false,
-      message: "Login failed",
+      message: "Profile Creation failed",
     };
   } catch (error: any) {
     const errorData = error.response?.data as ApiErrorResponse | undefined;
@@ -41,6 +41,42 @@ export const createProfile = async (
       success: false,
       message:
         errorData?.message || errorData?.detail || "Profile Creation failed",
+      detail: errorData?.detail,
+    };
+  }
+};
+
+export const getProfile = async (token: string): Promise<ProfileResult> => {
+  try {
+    const response = await axiosInstance.get<ProfileAuthenticatedResponse>(
+      "/profile/",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    );
+
+    const data = response.data;
+
+    if (response.status === 200 && data) {
+      return {
+        success: true,
+        data,
+      };
+    }
+
+    return {
+      success: false,
+      message: "Profile Fetch failed",
+    };
+  } catch (error: any) {
+    const errorData = error.response?.data as ApiErrorResponse | undefined;
+
+    return {
+      success: false,
+      message:
+        errorData?.message || errorData?.detail || "Profile fetch failed",
       detail: errorData?.detail,
     };
   }
